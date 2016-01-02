598d71f5f938952de9b152f9734401444682d563
Jeff Ward <jeffwardcoding@gmail.com>
2016-01-02 15:33:03 -0800

    wired up fader btns

--- js/PageFlow.js
+++ js/PageFlow.js
@@ -1,22 +1,41 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     //Controls the View of Salary OR Hourly
-    
     var SalaryMode = $('.SalaryMode');
     var HourlyMode = $('.HourlyMode');
-  
+
     $(SalaryMode).hide();
     $(HourlyMode).hide();
-    
-    $('.SalaryModeBtn').on('click', function(){
+    $('.ShowBtnArea').hide();
+
+    $('.SalaryModeBtn').on('click', function () {
         $(SalaryMode).show();
         $(HourlyMode).hide();
-        
     });
-      $('.HourlyModeBtn').on('click', function(){
+
+    $('.HourlyModeBtn').on('click', function () {
         $(SalaryMode).hide();
         $(HourlyMode).show();
-        
     });
-    
-})
+
+
+    //Toggles Button Hide
+    $('.hideBtn').on('click', function () {
+        $('.TypeSelectArea').fadeOut('fast');
+                $('.TypeSelectArea').hide();
+        $('.ShowBtnArea').fadeIn('slow');
+                $('.ShowBtnArea').show();
+
+    })
+
+    $('.showBtn').on('click', function () {
+        $('.ShowBtnArea').fadeOut('slow');
+        $('.ShowBtnArea').hide();
+        $('.TypeSelectArea').fadeIn('slow');
+
+
+    })
+
+
+
+});
